bfe2d9b4cfba0a4673d20190e9a1026c69e9190a
Alana Evey <Alana@Alanas-MacBook-Pro.local>
2019-03-10 21:23:20 -0600

    Inventory page in progress

--- Frontend/julie.js
+++ Frontend/julie.js
@@ -1,4 +1,55 @@
+/*
+var Email = localStorage.getItem("Email");
+if (Email == null || Email == "") {
+    $(document).ready(function () {
+        document.write("The page does not exist");
+    });
+} else {
 
+    $(document).ready(function () {
+       
+    
+    });
+}
+*/
+
+var makePostRequest = function(url, data, onSuccess, onFailure) {
+        $.ajax({
+            type: 'POST',
+            url: apiUrl + url,
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json",
+            success: onSuccess,
+            error: onFailure
+        });
+    };
+
+var makeGetRequest = function(url, onSuccess, onFailure) {
+       $.ajax({
+           type: 'GET',
+           url: apiUrl + url,
+           dataType: "json",
+           success: onSuccess,
+           error: onFailure
+       });
+   };
+
+var makeDeleteRequest = function (url, onSuccess, onFailure) {
+        $.ajax({
+            type: 'DELETE',
+            url: apiUrl + url,
+            success: onSuccess,
+            error: onFailure
+        });
+    };
+
+
+function Logout() 
+{
+alert("All changes are saved!");
+window.location.href = "./LogIn.html";
+};
 
 function on_addOrder()
 {
@@ -6,3 +57,4 @@ function on_addOrder()
     //need to update the Order and Customer tables
     // need to generate an order number
 }
+
